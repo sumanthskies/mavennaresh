7294c3d2716e264d9c8f464c364de2987d627338
sumanth <sumanth.singam@gmail.com>
2017-11-14 19:54:38 +0000

    new version of maven project

---
+++ src/main/java/com/mycompany/com/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
